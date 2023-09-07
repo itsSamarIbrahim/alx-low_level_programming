eca85f3ace83260c89516e300b112c1e8f7b8f4c
Samar <bantmasr21@gmail.com>
2023-09-07 15:38:57 -0700

    08.task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	sprintf("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	sprintf("and that piece of art is useful" "- Dora Korpar, 2015-10-19\n");
 	return (0);
 }
