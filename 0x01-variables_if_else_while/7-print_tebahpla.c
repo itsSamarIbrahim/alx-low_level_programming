36aaeeef772ff413b094c2ab1aa6f94a7a591185
Samar <bantmasr21@gmail.com>
2023-09-08 15:33:46 -0700

    07.task

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'z'; alphabet <= 'a'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	putchar(alphabet);
+
+	return (0);
+}
