36d06bfb15bd796b78ef938f029d88d27152e6fc
Samar <bantmasr21@gmail.com>
2023-09-08 16:07:35 -0700

    08.task

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,11 +12,11 @@ int main(void)
 
 	for (num = '0'; num <= '9'; num++)
 	{
-		putchar(num)
+		putchar(num);
 	}
 	for (ALPHA = 'A'; ALPHA <= 'F'; ALPHA++)
 	{
-		putchar(ALPHA)
+		putchar(ALPHA);
 	}
 	putchar('\n');
 
