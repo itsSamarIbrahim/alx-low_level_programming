f31b2d7a851fa95e6d9f187d5fa0153ea7fad40a
Samar <bantmasr21@gmail.com>
2023-09-08 16:28:45 -0700

    09.task

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,14 +9,14 @@ int main(void)
 {
     int num;
 
-    for (num = 0, num <= 9; num++)
-    {
-	putchar(num);
-    	if (num < 9)
+	for (num = 0, num <= 9; num++)
 	{
-		putchar(", ");
+	putchar(num);
+	if (num < 9)
+	{
+	putchar(", ");
 	}
-    }
+	}
     putchar('\n');
     return (0);
 }
