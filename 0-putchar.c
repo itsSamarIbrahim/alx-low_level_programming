bdb31650e48710fd2c7cf7a34e0bf3719c4f7562
Samar <bantmasr21@gmail.com>
2023-09-12 04:45:00 -0700

    00.task

---
+++ 0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* main - Write a program that prints _putchar, followed by a new line.
+ *
+ * return: returns 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
