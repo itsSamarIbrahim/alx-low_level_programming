0c20a5b0a000f7a448d6a89897ff4e6cee59f0f7
Samar <bantmasr21@gmail.com>
2023-09-12 12:49:33 -0700

    05.task

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,6 +3,8 @@
 /**
  * print_sign - a function that prints the sign of a number.
  *
+ * @n: the number to be checked.
+ *
  * Return: 1, 0 or -1
  */
 int print_sign(int n)
@@ -10,7 +12,7 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		return (1);	
+		return (1);
 	}
 	else if (n < -1)
 	{
