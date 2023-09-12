f1decf51a02a458b1fcdebffb6013181c7b1e522
Samar <bantmasr21@gmail.com>
2023-09-12 11:03:34 -0700

    03.task

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - a function that checks for lowercase character. 
+ *
+ * return : 0
+ */
+int _islower(int c)
+{
+	char letter;
+
+	if (_islower(letter = c))
+		return (1);
+	else
+		return (0);
+}
