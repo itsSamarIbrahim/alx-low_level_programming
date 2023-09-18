1f47a0f587107531c464dc303e1a6e781f3b3439
Samar <bantmasr21@gmail.com>
2023-09-18 14:11:28 -0700

    08.task

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,12 +14,12 @@ void print_array(int *a, int n)
 
 	for (count = 0; count < n; count++)
 	{
-	       if (count == (n - 1))
-	       {
-		       printf("%d", a[count]);
-	       }	       
-	       else
-		       printf("%d, ", a[count]);
+		if (count == (n - 1))
+		{
+			printf("%d", a[count]);
+		}
+		else
+			printf("%d, ", a[count]);
 	}
 	printf("\n");
 }
