e9da04a50b7ad9986caed4f5869d125e3c80f22c
Samar <bantmasr21@gmail.com>
2023-09-21 15:30:01 -0700

    08.task

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -11,9 +11,11 @@ char *rot13(char *str)
 
 	while (str[i] != '\0')
 	{
-		if ((str[i] >= 'A' && str[i] <= 'M') || (str[i] >= 'a' && str[i] <= 'm'))
+		if ((str[i] >= 'A' && str[i] <= 'M') ||
+				(str[i] >= 'a' && str[i] <= 'm'))
 			str[i] = str[i] + 13;
-		else if ((str[i] >= 'N' && str[i] <= 'Z') || (str[i] >= 'n' && str[i] <= 'z'))
+		else if ((str[i] >= 'N' && str[i] <= 'Z') ||
+				(str[i] >= 'n' && str[i] <= 'z'))
 			str[i] = str[i] - 13;
 		i++;
 	}
