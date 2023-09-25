11ada1d02ef46d97ef0382711fa85b37f51333ce
Samar <bantmasr21@gmail.com>
2023-09-25 13:07:36 -0700

    05.task

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	if (*needle == '\0')
+	{
+		return (haystack);
+	}
+	while (*haystack != '\0')
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*n != '\0' && *h == *n)
+		{
+			h++;
+			n++;
+		}
+		if (*n == '\0')
+		{
+			return (haystack);
+		}
+		haystack++;
+	}
+
+	return (0);
+}
