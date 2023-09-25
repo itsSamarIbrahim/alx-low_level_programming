cc9fca0433e8f778850b3ddb13a67558b74c8f3a
Samar <bantmasr21@gmail.com>
2023-09-25 08:34:12 -0700

    02.task

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,16 +8,13 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i = 0;
-
-	while (s[i] != '\0')
+	while (*s != '\0')
 	{
-		if (s[i] == c)
+		s++;
+		if (*s == c)
 		{
 			return (s);
-			i++;
 		}
-	s++;
 	}
 
 	return (0);
