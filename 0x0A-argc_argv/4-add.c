e2f7f6298780ffd38fe061e83ad3fb2ab7dd06c4
Samar <bantmasr21@gmail.com>
2023-10-02 14:15:48 -0700

    04.task

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,6 +13,11 @@ int main(int argc, char *argv[])
 	int a;
 	int sum = 0;
 
+	 if (argc == 1)
+        {
+                printf("0\n");
+                return (0);
+        }
 	for (a = 1; a < argc; a++)
 	{
 		if (!isdigit(*argv[a]))
