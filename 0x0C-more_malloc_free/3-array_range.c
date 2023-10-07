4f9c84e2f3ff18698a3b949b480dd759dc0ac261
Samar <bantmasr21@gmail.com>
2023-10-07 11:04:55 -0700

    03.task

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *array_range -  a function that creates an array of integers
+ * @min: the smallest value
+ * @max: the largest value
+ * Return: returns a pointer to a dynamically allocated array of
+ * integers containing values from min to max (ON SUCCESS)
+ * if min is greater than max or if memory allocation fails,
+ * it returns NULL (ON FAILURE)
+ */
+int *array_range(int min, int max)
+{
+	int *prod, i, range;
+
+	if (min > max)
+		return (NULL);
+
+	range = max - min + 1;
+	prod = (int *)malloc(sizeof(int) * range);
+
+	if (prod == NULL)
+		return (NULL);
+	for (i = 0; i < max; i++)
+		prod[i] = min + i;
+
+	return (prod);
+}
