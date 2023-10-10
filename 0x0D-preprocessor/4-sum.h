f99a0b3e2a0fe625c051de9b65dee958eac1b330
Samar <bantmasr21@gmail.com>
2023-10-10 06:25:18 -0700

    04.task

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -4,6 +4,8 @@
 /**
  * SUM_H - a function-like macro SUM(x, y) that computes
  * the sum of the numbers x and y.
+ * @x: the first number to be added
+ * @y: the second number to be added
  */
 #define SUM(x, y) ((x) + (y))
 
