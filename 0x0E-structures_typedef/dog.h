e57665415175f58d978af567563ee9dff0c8f016
Samar <bantmasr21@gmail.com>
2023-10-10 11:46:00 -0700

    00.task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,24 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog {
+/**
+ * struct dog - a data structure
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the name of the owner of the dog
+ */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
