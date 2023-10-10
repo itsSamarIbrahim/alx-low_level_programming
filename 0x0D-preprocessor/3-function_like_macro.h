8d9dc6a6512409e1d3618e126f6f70b2550e64e3
Samar <bantmasr21@gmail.com>
2023-10-10 06:17:17 -0700

    03.task

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+ * FUNCTION_LIKE_MACRO-H - a function-like macro ABS(x) that
+ * computes the absolute value of a number x
+ */
+#define ABS(x) ((x) < (0) ? (-x) : (x))
+
+#endif
