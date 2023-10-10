f6b1e50f3843f7013cef2ae2c2eed4337681d980
Samar <bantmasr21@gmail.com>
2023-10-10 05:53:41 -0700

    02.task

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * 2-main.c - a program that prints the name of the file it was compiled from
+ * main - a program that prints the name of the file it was compiled from
  * Return: 0 (ON SUCCESS)
  */
 int main(void)
