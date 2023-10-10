643c630420cf4a6944090d513571bd00f7d2abb8
Samar <bantmasr21@gmail.com>
2023-10-10 13:11:57 -0700

    01.task

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,15 +3,15 @@
 /**
  * init_dog - a function initializes the members of
  * a struct dog with the provided values
- * @*d: a pointer to struct dog
- * @*name; a pointer to the dog's name
+ * @d: a pointer to struct dog
+ * @name; a pointer to the dog's name
  * @age: the age of the dog
- * @*owner: the owner of the dog
+ * @owner: the owner of the dog
  * Return: void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if(d)
+	if (d)
 	{
 		d->name = name;
 		d->age = age;
