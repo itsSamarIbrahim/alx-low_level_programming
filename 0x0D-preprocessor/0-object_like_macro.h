08b228ac3ce8a5dd179afc5bc758aa4b03cc9cc4
Samar <bantmasr21@gmail.com>
2023-10-10 05:38:56 -0700

    00.task

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,11 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
 #define SIZE 1024
 
 /**
  * object_like_macro.h - a header file that defines a macro named SIZE
  * as an abbreviation for the token 1024
  */
+
+#endif
