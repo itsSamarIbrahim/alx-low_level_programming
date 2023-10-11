25026234f320f122158d2be1c1f8a12ebfe202b1
Samar <bantmasr21@gmail.com>
2023-10-11 16:44:28 -0700

    03.task

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	if (atoi(argv[3]) == '0' && (atoi(argv[2]) == '/' || atoi(argv[2]) == '%'))
+	if (argv[3][0] == '0' && (argv[2][0] == '/' || argv[2][0] == '%'))
 	{
 		printf("Error\n");
 		exit(100);
