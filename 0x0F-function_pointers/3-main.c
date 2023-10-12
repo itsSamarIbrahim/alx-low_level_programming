71d156497541400367e7d79022607f205b47934d
Samar <bantmasr21@gmail.com>
2023-10-11 17:33:41 -0700

    03.task

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -23,11 +23,13 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	/* if (argv[3][0] == '0' && (argv[2][0] == '/' || argv[2][0] == '%'))
-	{
-		printf("Error\n");
-		exit(100);
-	} */
+	/**
+	 * if (argv[3][0] == '0' && (argv[2][0] == '/' || argv[2][0] == '%'))
+	 * {
+	 * printf("Error\n");
+	 * exit(100);
+	 * }
+	 */
 
 	printf("%d\n", result(num1, num2));
 
