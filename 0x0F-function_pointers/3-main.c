133c17522eecd04f166062c59b0140164a1f7501
Samar <bantmasr21@gmail.com>
2023-10-11 18:13:50 -0700

    03.task

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -16,20 +16,19 @@ int main(int argc, char *argv[])
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 
-	result = get_op_func(argv[2]);
-
-	if (result == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
-
 	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
+
+        result = get_op_func(argv[2]);
+
+        if (result == NULL)
+        {
+                printf("Error\n");
+                exit(99);
+        }
 
 	printf("%d\n", result(num1, num2));
 
