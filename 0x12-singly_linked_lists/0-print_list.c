a4c5a3a0c8a847f0f34de9919a47b96cdbe08f26
Samar <bantmasr21@gmail.com>
2023-10-19 13:27:09 -0700

    00.task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,11 +7,11 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t counter;
+	size_t counter = 0;
 
 	while (h != NULL)
 	{
-		if ((*h).str != NULL)
+		if ((*h).str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", (*h).len, (*h).str);
