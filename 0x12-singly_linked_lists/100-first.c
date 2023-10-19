a40f6ca8e392af3cf671d31884069e7bcbb8158d
Samar <bantmasr21@gmail.com>
2023-10-19 16:47:18 -0700

    05.task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * beforeMain - a function that prints a sentence
+ * before tha main function is executed
+ * Return: void
+ */
+void beforeMain(void)
+{
+	printf("ou're beat! and yet, you must allow,\n
+	I bore my house upon my back!\n");
+}
