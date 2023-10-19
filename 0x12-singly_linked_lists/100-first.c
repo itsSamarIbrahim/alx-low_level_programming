aa44fdbc455a2fab10ef6a3dfd65a64d318ec34f
Samar <bantmasr21@gmail.com>
2023-10-19 16:49:46 -0700

    05.task

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,6 +7,6 @@
  */
 void beforeMain(void)
 {
-	printf("ou're beat! and yet, you must allow,\n
-	I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
