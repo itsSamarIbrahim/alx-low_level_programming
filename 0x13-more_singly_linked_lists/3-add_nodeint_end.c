fb2fc067db2506470b09a74856416e61f1cb73b2
Samar <bantmasr21@gmail.com>
2023-10-24 08:11:05 -0700

    03.task

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,7 +9,7 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *newEndNode;
+	listint_t *newEndNode, *current;
 
 	newEndNode = malloc(sizeof(listint_t));
 	if (newEndNode == NULL)
@@ -22,7 +22,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		*head = newEndNode;
 	else
 	{
-		listint_t *current = *head;
+		current = *head;
 		while ((*current).next)
 			current = (*current).next;
 		(*current).next = newEndNode;
