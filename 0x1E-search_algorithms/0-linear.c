d30b0f18916b74d8b35da18117effc97646a5075
Samar <bantmasr21@gmail.com>
2024-05-22 10:53:47 -0700

    0-linear

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -2,14 +2,14 @@
 
 /**
  * linear_search - Performs a linear search on an integer array to find
- *                 a given value
+ * a given value
  *
  * @array: Pointer to the start of the integer array
  * @size: The total number of elements in the array
  * @value: The integer value to search for within the array
  *
  * Return: The index of the found value in the array, or -1 if the
- *         value is not found
+ * value is not found
  */
 int linear_search(int *array, size_t size, int value)
 {
