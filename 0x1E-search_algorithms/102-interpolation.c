06b65a5b2ae801c7c23718af5188d029f574aebc
Samar <bantmasr21@gmail.com>
2024-05-22 13:43:17 -0700

    102-interpolation.c

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -21,34 +21,27 @@ int interpolation_search(int *array, size_t size, int value)
 	while (low_index <= high_index && value >= array[low_index]
 			&& value <= array[high_index])
 	{
-		if (low_index == high_index)
-		{
-			if (array[low_index] == value)
-			{
-				printf("Value checked array[%lu] = [%d]\n",
-						low_index, array[low_index]);
-				return (low_index);
-			}
-			return (-1);
-		}
 		pos = low_index + (((double)(high_index - low_index) /
 					(array[high_index]
 					 - array[low_index])) * (value
 					 - array[low_index]));
-		if (pos >= size)
+		if (pos < size)
 		{
-			printf("Value checked array[%lu] is out of range\n",
+			printf("Value checked array[%ld] = [%d]\n",
+					pos, array[pos]);
+			if (array[pos] == value)
+				return (pos);
+			else if (array[pos] < value)
+				low_index = pos + 1;
+			else
+				high_index = pos - 1;
+		}
+		else
+		{
+			printf("Value checked array[%ld] is out of range\n",
 					pos);
 			return (-1);
 		}
-		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
-		if (array[pos] == value)
-			return (pos);
-
-		if (array[pos] < value)
-			low_index = pos + 1;
-		else
-			high_index = pos - 1;
 	}
 
 	return (-1);
